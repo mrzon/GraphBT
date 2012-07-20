a59a0bc91435a705cbc78fa8f24baf743b6ff1e6
AbieIkhsan <abie.ikhsan@gmail.com>
2012-07-20 18:26:27 +0700

    Added initial phase of property

---
+++ BehaviorTreeModel/src/behaviortree/graphBT/property/BehaviorTreeFilter.java
@@ -0,0 +1,51 @@
+package behaviortree.graphBT.property;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.graphiti.services.Graphiti;
+import org.eclipse.graphiti.ui.platform.AbstractPropertySectionFilter;
+import org.eclipse.graphiti.mm.pictograms.Shape;
+import behaviortree.*;
+
+
+public class BehaviorTreeFilter extends AbstractPropertySectionFilter {
+	
+    @Override
+    protected boolean accept(PictogramElement pe) {
+        EObject eObject =
+            Graphiti.getLinkService()
+            .getBusinessObjectForLinkedPictogramElement(pe);
+        EObject eObj =
+                Graphiti.getLinkService()
+                .getBusinessObjectForLinkedPictogramElement(((Shape)pe).getContainer());
+            
+        
+        System.out.println("Ane masuk filter nyakkk");
+        boolean isTrue = false;
+        if (eObject instanceof StandardNode) {
+        	isTrue = true;
+        	System.out.println("Ini adalah standard node di treefilter..");
+        }
+        if (eObject instanceof Component) {
+        	isTrue = true;
+        	System.out.println("Ini adalah component di treefilter..");
+        }
+        if (eObject instanceof Behavior) {
+        	isTrue = true;
+        	System.out.println("Ini adalah behavior di treefilter..");
+        }
+        if (eObj instanceof StandardNode) {
+        	isTrue = true;
+        	System.out.println("Ini adalah standard node..");
+        }
+        if (eObj instanceof Component) {
+        	isTrue = true;
+        	System.out.println("Ini adalah component..");
+        }
+        if (eObj instanceof Behavior) {
+        	isTrue = true;
+        	System.out.println("Ini adalah behavior..");
+        }
+        return isTrue;
+    }
+}
---
+++ BehaviorTreeModel/src/behaviortree/graphBT/property/BehaviorTreePropertySection.java
@@ -0,0 +1,152 @@
+package behaviortree.graphBT.property;
+
+import org.eclipse.graphiti.features.IFeature;
+import org.eclipse.graphiti.features.context.IContext;
+import org.eclipse.graphiti.features.context.impl.CustomContext;
+import org.eclipse.graphiti.features.impl.AbstractFeature;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.graphiti.services.Graphiti;
+import org.eclipse.graphiti.ui.platform.GFPropertySection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.views.properties.tabbed.ITabbedPropertyConstants;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetWidgetFactory;
+
+import behaviortree.*;
+
+public class BehaviorTreePropertySection extends GFPropertySection 
+	implements ITabbedPropertyConstants {
+	private Text componentText;
+	private Text behaviorText;
+	
+
+    @Override
+    public void createControls(Composite parent, 
+    		TabbedPropertySheetPage tabbedPropertySheetPage) {
+		super.createControls(parent, tabbedPropertySheetPage);
+
+        TabbedPropertySheetWidgetFactory factory = getWidgetFactory();
+        Composite composite = factory.createFlatFormComposite(parent);
+
+        FormData componentData;
+        FormData behaviorData;
+
+        componentText = factory.createText(composite, "");
+        behaviorText = factory.createText(composite, "");
+        
+        componentText.addModifyListener(modifyListener());
+        
+        componentData = new FormData();
+        componentData.left = new FormAttachment(0, STANDARD_LABEL_WIDTH);
+        componentData.right = new FormAttachment(100, 0);
+        componentData.top = new FormAttachment(0, VSPACE);
+        componentText.setLayoutData(componentData);
+
+        CLabel valueLabel = factory.createCLabel(composite, "Component:");
+        componentData = new FormData();
+        componentData.left = new FormAttachment(0, 0);
+        componentData.right = new FormAttachment(componentText, -HSPACE);
+        componentData.top = new FormAttachment(componentText, 0, SWT.CENTER);
+        valueLabel.setLayoutData(componentData);
+        
+
+        behaviorData = new FormData();
+        behaviorData.left = new FormAttachment(0, STANDARD_LABEL_WIDTH);
+        behaviorData.right = new FormAttachment(100, 0);
+        behaviorData.top = new FormAttachment(0, VSPACE);
+        behaviorText.setLayoutData(behaviorData);
+
+        CLabel valueLabel2 = factory.createCLabel(composite, "Behavior:");
+        behaviorData = new FormData();
+        behaviorData.left = new FormAttachment(0, 0);
+        behaviorData.right = new FormAttachment(componentText, -HSPACE);
+        behaviorData.top = new FormAttachment(componentText, 0, SWT.CENTER);
+        valueLabel.setLayoutData(behaviorData);
+
+    }
+
+    @Override
+    public void refresh() {
+        PictogramElement pe = getSelectedPictogramElement();
+
+        if (pe != null) {
+            Object bo = Graphiti.getLinkService()
+                 .getBusinessObjectForLinkedPictogramElement(pe);
+
+            if (bo == null)
+                return;
+            
+            componentText.addModifyListener(modifyListener());
+        }
+    }
+    
+    private ModifyListener modifyListener() {
+    	return new ModifyListener(){
+	    	@Override
+	    	public void modifyText(ModifyEvent e) {
+	    		String value = componentText.getText();
+	    		if (value == null) {
+	    			value = "";
+	    		}
+	    		PictogramElement pe = getSelectedPictogramElement();
+	    		System.out.println("Ini objeknya punya tipenya apa eaaa");
+	    		if (pe != null) {
+	    			Object bo = Graphiti.getLinkService().
+	    					getBusinessObjectForLinkedPictogramElement(pe);
+	    			if (bo == null)
+	    				return;
+	    			String name = ((StandardNode) bo).getComponent().getComponentName();
+	    			System.out.println("Component: " + name);
+	    			if (value.equals(name))
+	    				return;
+	    		}
+	    		final String typedValue = value;
+	    		IFeature feature = new AbstractFeature(
+	    				getDiagramTypeProvider().getFeatureProvider()) {
+	    			@Override
+	    			public void execute(IContext context) {
+	    				PictogramElement pe = getSelectedPictogramElement();
+	    				if (pe != null) {
+	    					Object bo = Graphiti.getLinkService().
+	    							getBusinessObjectForLinkedPictogramElement(pe);
+	    					
+	    					if (bo == null)
+	    						return;
+	    					
+	    					if(bo instanceof StandardNode)
+	    					{
+	    						StandardNode node = (StandardNode) bo;
+	    						System.out.println("StandardNode...");
+	    						node.getComponent().setComponentName(typedValue);
+	    					}
+	    					if(bo instanceof Component)
+	    					{
+	    						Component eClass = (Component) bo;
+	    						System.out.println("Component...");
+	    					}
+	    					if(bo instanceof Behavior)
+	    					{
+	    						Behavior eClass = (Behavior) bo;
+	    						System.out.println("behavior...");
+	    					}
+	    				}
+	    			}
+	    			
+	    			@Override
+	    			public boolean canExecute(IContext context) {
+	    				return true;
+	    			}
+	    		};
+	    		CustomContext context = new CustomContext();
+	    		execute(feature, context);
+	    	}
+	    };
+    }
+}
